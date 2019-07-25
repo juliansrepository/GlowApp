f7c17d91bee639071198cf94b13c616bd9dda0f8
Julian Alarcon <jalar015@fiu.edu>
2019-07-25 13:41:01 -0400

    Testing use of jQuery AJAX to POST the form data for future query

--- Glow.py
+++ Glow.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, json
+import os
+import main_functions
 app = Flask(__name__)
 
 #Adds home page
@@ -8,10 +9,26 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/serialize', methods=['POST'])
+def serialize():
+    user =  request.form['username'];
+    return main_functions.save_to_file(user, 'query.json')
+        #json.dumps({'status':'OK','user':user,'pass':password});
+
+
+""""
 #Adds about page
 @app.route('/about')
 def about():
     return render_template('about.html')
+"""
 
 #Debug mode is turned on
 if __name__ == "__main__":
